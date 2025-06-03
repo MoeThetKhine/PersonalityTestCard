d916312b4334798be35226ac9494bc3dd1a637c2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-03 21:35:19 +0630

    Feat : Add TblUserElementScore

---
+++ DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblUserElementScore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+
+public partial class TblUserElementScore
+{
+    public int UserElementId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int ElementId { get; set; }
+
+    public int Score { get; set; }
+
+    public virtual TblElement Element { get; set; } = null!;
+
+    public virtual TblUser User { get; set; } = null!;
+}
