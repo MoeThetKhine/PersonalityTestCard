d673d27a72046e065ca39d2ee8a1eccbb658e112
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-03 21:34:53 +0630

    Feat : Add Tbluser

---
+++ DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+
+public partial class TblUser
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<TblUserCard> TblUserCards { get; set; } = new List<TblUserCard>();
+
+    public virtual ICollection<TblUserElementScore> TblUserElementScores { get; set; } = new List<TblUserElementScore>();
+}
