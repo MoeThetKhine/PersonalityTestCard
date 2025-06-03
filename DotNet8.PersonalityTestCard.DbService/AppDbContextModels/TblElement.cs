fff438ea2896c77b8eb4ddbe0ced3be725dbf33a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-03 21:34:41 +0630

    Feat : Add TblElement

---
+++ DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+
+public partial class TblElement
+{
+    public int ElementId { get; set; }
+
+    public string ElementName { get; set; } = null!;
+
+    public virtual ICollection<TblCard> TblCards { get; set; } = new List<TblCard>();
+
+    public virtual ICollection<TblUserElementScore> TblUserElementScores { get; set; } = new List<TblUserElementScore>();
+}
