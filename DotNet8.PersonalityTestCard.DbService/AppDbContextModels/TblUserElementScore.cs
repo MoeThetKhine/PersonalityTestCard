3a63f1749b9ef84b5fef8593b005db2c7b5b8ac6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-03 21:38:44 +0630

    -

--- DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblUserElementScore.cs
+++ DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblUserElementScore.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+﻿namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
 
 public partial class TblUserElementScore
 {
