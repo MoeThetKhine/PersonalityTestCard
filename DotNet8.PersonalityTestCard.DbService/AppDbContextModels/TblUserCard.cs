e8dce40785274ea568f72f042aaed7904adc4ab7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-03 21:35:04 +0630

    Feat : Add TblUser

---
+++ DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblUserCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+
+public partial class TblUserCard
+{
+    public int UserCardId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int CardId { get; set; }
+
+    public virtual TblCard Card { get; set; } = null!;
+
+    public virtual TblUser User { get; set; } = null!;
+}
