127ecdd96a0d2d0fd576676b77857b70cb02cc43
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-03 21:34:28 +0630

    Feat : Add TblCard

---
+++ DotNet8.PersonalityTestCard.DbService/AppDbContextModels/TblCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+
+public partial class TblCard
+{
+    public int CardId { get; set; }
+
+    public string CardName { get; set; } = null!;
+
+    public int ElementId { get; set; }
+
+    public virtual TblElement Element { get; set; } = null!;
+
+    public virtual ICollection<TblUserCard> TblUserCards { get; set; } = new List<TblUserCard>();
+}
