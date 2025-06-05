2f395d986317ca4c642055b79d8057135c831b55
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-05 22:32:26 +0630

    -

---
+++ DotNet8.PersonalityTestCard.Api/Controllers/Element/ElementController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.PersonalityTestCard.Api.Controllers.Element
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ElementController : ControllerBase
+	{
+	}
+}
