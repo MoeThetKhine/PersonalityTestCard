6b8f6ef5b3e7ccec06af040687e38e59d04c1553
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-05 22:32:41 +0630

    -

---
+++ DotNet8.PersonalityTestCard.Api/Controllers/User/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.PersonalityTestCard.Api.Controllers.User
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+	}
+}
