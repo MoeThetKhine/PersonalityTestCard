b7ebc7352699d6ab82ae3b897060e9ae56e70186
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:28:24 +0630

    Feat : Add CardRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/Card/CardRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Card/CardRepository.cs
@@ -1,8 +1,40 @@
 ﻿using DotNet8.PersonalityTestCard.Api.Repositories.Card;
+using DotNet8.PersonalityTestCard.Models.Setup.Card;
+using DotNet8.PersonalityTestCard.Models;
 
 namespace DotNet8.PersonalityTestCard.Api.Repositories.Blog
 {
 	public class CardRepository : ICardRepository
 	{
+
+		private readonly AppDbContext _appDbContext;
+
+		public CardRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<CardListResponseModel> GetCardsAsync()
+		{
+			try
+			{
+				var dataLst = await _appDbContext.TblCards
+					 .AsNoTracking()
+					 .OrderBy(x => x.CardName)
+					 .ToListAsync();
+
+				var lst = dataLst.Select(x => x.Change()).ToList();
+
+				CardListResponseModel responseModel = new()
+				{
+					DataLst = lst
+				};
+				return responseModel;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
 	}
 }
