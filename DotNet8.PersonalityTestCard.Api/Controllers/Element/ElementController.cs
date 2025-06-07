650c1e8dc99213791289359896dcf2e4a76f8d31
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 00:06:47 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Controllers/Element/ElementController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/Element/ElementController.cs
@@ -1,37 +1,36 @@
 ﻿using DotNet8.PersonalityTestCard.Api.Features.Element.Queries.GetElementList;
 
-namespace DotNet8.PersonalityTestCard.Api.Controllers.Element
+namespace DotNet8.PersonalityTestCard.Api.Controllers.Element;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ElementController : BaseController
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class ElementController : BaseController
+	private readonly IMediator _mediator;
+
+	public ElementController(IMediator mediator)
 	{
-		private readonly IMediator _mediator;
-
-		public ElementController(IMediator mediator)
-		{
-			_mediator = mediator;
-		}
-
-		#region GetElementAsync
-
-		[HttpGet]
-		public async Task<IActionResult> GetElementAsync()
-		{
-			try
-			{
-				var query = new GetElementListQuery();
-				var lst = await _mediator.Send(query);
-
-				return Content(lst);
-			}
-
-			catch (Exception ex)
-			{
-				return InternalServerError(ex);
-			}
-		}
-
-		#endregion
+		_mediator = mediator;
 	}
+
+	#region GetElementAsync
+
+	[HttpGet]
+	public async Task<IActionResult> GetElementAsync()
+	{
+		try
+		{
+			var query = new GetElementListQuery();
+			var lst = await _mediator.Send(query);
+
+			return Content(lst);
+		}
+
+		catch (Exception ex)
+		{
+			return InternalServerError(ex);
+		}
+	}
+
+	#endregion
 }
