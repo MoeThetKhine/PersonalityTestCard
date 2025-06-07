f28ddf2b64232fccea8b47b17a16495cbaac761a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 12:10:15 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Controllers/BaseController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/BaseController.cs
@@ -1,32 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace DotNet8.PersonalityTestCard.Api.Controllers
+namespace DotNet8.PersonalityTestCard.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BaseController : ControllerBase
+
+	protected IActionResult Content(object? obj)
 	{
-
-		protected IActionResult Content(object? obj)
-		{
-			return Content(JsonConvert.SerializeObject(obj), "application/json"); 
-		}
-
-		protected IActionResult InternalServerError(Exception ex)
-		{
-			return StatusCode(500, ex.Message);
-		}
-
-		protected IActionResult Created(string message)
-		{
-			return StatusCode(201, message);
-		}
-
-		protected IActionResult Accepted(string message)
-		{
-			return StatusCode(202, message);
-		}
-
+		return Content(JsonConvert.SerializeObject(obj), "application/json"); 
 	}
+
+	protected IActionResult InternalServerError(Exception ex)
+	{
+		return StatusCode(500, ex.Message);
+	}
+
+	protected IActionResult Created(string message)
+	{
+		return StatusCode(201, message);
+	}
+
+	protected IActionResult Accepted(string message)
+	{
+		return StatusCode(202, message);
+	}
+
 }
