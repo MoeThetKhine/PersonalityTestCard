943ec59c633198aabad4be39b8f46e403f9ee9ac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 21:19:29 +0630

    Feat : Add ElementListResponseModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/Element/ElementListResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.Element
+{
+	public class ElementListResponseModel
+	{
+		public List<ElementListModel> DataLst { get; set; }
+	}
+}
