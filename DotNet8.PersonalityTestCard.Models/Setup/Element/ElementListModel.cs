47ae1cce50e7a42aa3260bfdaa8451c926576047
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 21:14:24 +0630

    Feat : Add ElementListModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/Element/ElementListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.Element
+{
+	public class ElementListModel
+	{
+		public int ElementId { get; set; }
+
+		public string ElementName { get; set; } = null!;
+	}
+}
