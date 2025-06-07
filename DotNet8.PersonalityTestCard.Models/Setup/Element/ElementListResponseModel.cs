bd4bee8d5c1a088109218e88d2435906bec1474a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 21:19:38 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/Element/ElementListResponseModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/Element/ElementListResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.PersonalityTestCard.Models.Setup.Element
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.Element
 {
 	public class ElementListResponseModel
 	{
