eea1660f5449e51839160c2af5401ced0e38c4ef
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 18:19:17 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/ModularService.cs
+++ DotNet8.PersonalityTestCard.Api/ModularService.cs
@@ -1,48 +1,47 @@
-﻿namespace DotNet8.PersonalityTestCard.Api
+﻿namespace DotNet8.PersonalityTestCard.Api;
+
+public static class ModularService
 {
-	public static class ModularService
+
+	public static IServiceCollection AddService(this IServiceCollection services, WebApplicationBuilder builder)
 	{
+		services.AddRepositoryServices()
+		.AddDbContextService(builder)
+		.AddMediatRService()
+		.AddJsonServices();
 
-		public static IServiceCollection AddService(this IServiceCollection services, WebApplicationBuilder builder)
+		return services;
+	}
+
+	private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+	{
+		services.AddScoped<ICardRepository, CardRepository>();
+		return services;
+	}
+
+	private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		builder.Services.AddDbContext<AppDbContext>(opt =>
 		{
-			services.AddRepositoryServices()
-			.AddDbContextService(builder)
-			.AddMediatRService()
-			.AddJsonServices();
+			opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+		}, ServiceLifetime.Transient);
 
-			return services;
-		}
+		return services;
+	}
 
-		private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+	private static IServiceCollection AddMediatRService(this IServiceCollection services)
+	{
+		services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Program).Assembly));
+		return services;
+	}
+
+	private static IServiceCollection AddJsonServices(this IServiceCollection services)
+	{
+		services.AddControllers().AddJsonOptions(opt =>
 		{
-			services.AddScoped<ICardRepository, CardRepository>();
-			return services;
-		}
+			opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+		});
 
-		private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			builder.Services.AddDbContext<AppDbContext>(opt =>
-			{
-				opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-			}, ServiceLifetime.Transient);
-
-			return services;
-		}
-
-		private static IServiceCollection AddMediatRService(this IServiceCollection services)
-		{
-			services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Program).Assembly));
-			return services;
-		}
-
-		private static IServiceCollection AddJsonServices(this IServiceCollection services)
-		{
-			services.AddControllers().AddJsonOptions(opt =>
-			{
-				opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-			});
-
-			return services;
-		}
+		return services;
 	}
 }
