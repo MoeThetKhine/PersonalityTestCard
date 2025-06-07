d40b823a96fbf84575ea9379ffa05e1a631aef44
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 10:37:56 +0630

    Feat : Add CardListModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/Card/CardListModel.cs
@@ -0,0 +1,20 @@
+﻿using DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.Card
+{
+	public class CardListModel
+	{
+		public int CardId { get; set; }
+
+		public string CardName { get; set; } = null!;
+
+		public int ElementId { get; set; }
+
+
+	}
+}
