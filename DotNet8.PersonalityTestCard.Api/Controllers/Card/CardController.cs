ee5dbc1c0bef9342112a88479bdb5bb609eec953
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:29:46 +0630

    Feat : Add GetCardAsync in Controller

--- DotNet8.PersonalityTestCard.Api/Controllers/Card/CardController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/Card/CardController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet8.PersonalityTestCard.Api.Features.Card.Queries.GetCardList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.PersonalityTestCard.Api.Controllers.Card
 {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class CardController : ControllerBase
+	public class CardController : BaseController
 	{
+		private readonly IMediator _mediator;
+
+		public CardController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetCardAsync()
+		{
+			try
+			{
+				var query = new GetCardListQuery();
+				var lst = await _mediator.Send(query);
+
+				return Content(lst);
+			}
+			catch (Exception ex)
+			{
+				return InternalServerError(ex);
+			}
+		}
 	}
 }
