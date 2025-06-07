cf0e4e9ff42d3fbfada6ad400b61c53319314a52
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 10:38:07 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/Card/CardListModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/Card/CardListModel.cs
@@ -1,11 +1,4 @@
-﻿using DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.PersonalityTestCard.Models.Setup.Card
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.Card
 {
 	public class CardListModel
 	{
