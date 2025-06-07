115349c1a5d4dec58566ed6c313f65336020a070
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:29:05 +0630

    Feat : Add GetCardListQueryHandler

--- DotNet8.PersonalityTestCard.Api/Features/Card/Queries/GetCardList/GetCardListQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/Card/Queries/GetCardList/GetCardListQueryHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Features.Card.Queries.GetCardList
+﻿using DotNet8.PersonalityTestCard.Models.Setup.Card;
+using MediatR;
+
+namespace DotNet8.PersonalityTestCard.Api.Features.Card.Queries.GetCardList
 {
-	public class GetCardListQueryHandler
+	public class GetCardListQueryHandler : IRequestHandler<GetCardListQuery, CardListResponseModel>
 	{
+		private readonly ICardRepository _cardRepository;
+
+		public GetCardListQueryHandler(ICardRepository cardRepository)
+		{
+			_cardRepository = cardRepository;
+		}
+
+		public async Task<CardListResponseModel> Handle(GetCardListQuery request, CancellationToken cancellationToken)
+		{
+
+			return await _cardRepository.GetCardsAsync();
+		}
 	}
 }
