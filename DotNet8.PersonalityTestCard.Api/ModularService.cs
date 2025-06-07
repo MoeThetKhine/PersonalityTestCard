32086fd23ba685a7b335fd9c538d5e14ee6a997d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 17:31:51 +0630

    Feat : Add AddRepositoryServices

---
+++ DotNet8.PersonalityTestCard.Api/ModularService.cs
@@ -0,0 +1,21 @@
+﻿using DotNet8.PersonalityTestCard.Api.Repositories.Blog;
+using DotNet8.PersonalityTestCard.Api.Repositories.Card;
+
+namespace DotNet8.PersonalityTestCard.Api
+{
+	public static class ModularService
+	{
+
+		public static IServiceCollection AddService(this IServiceCollection services, WebApplicationBuilder builder)
+		{
+	
+		}
+
+		private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+		{
+			services.AddScoped<ICardRepository, CardRepository>();
+			return services;
+		}
+
+	}
+}
