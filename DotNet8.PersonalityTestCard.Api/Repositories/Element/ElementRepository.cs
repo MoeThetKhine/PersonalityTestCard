216c4a72512f53099391d34dd9fc8b15d2c266a2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 23:53:26 +0630

    Feat : Add ElementRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/Element/ElementRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Element/ElementRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.Element
+﻿
+namespace DotNet8.PersonalityTestCard.Api.Repositories.Element
 {
 	public class ElementRepository : IElementRepository
 	{
+		private readonly AppDbContext _appDbContext;
+
+		public ElementRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<ElementListResponseModel> GetElementAsync()
+		{
+			try
+			{
+				var dataLst = await _appDbContext.TblElements
+					.AsNoTracking()
+					.OrderByDescending(x => x.ElementId)
+					.ToListAsync();
+
+				var lst = dataLst.Select(x => x.Change()).ToList();
+
+				ElementListResponseModel responseModel = new()
+				{
+					DataLst = lst
+				};
+
+				return responseModel;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+
+		}
 	}
 }
