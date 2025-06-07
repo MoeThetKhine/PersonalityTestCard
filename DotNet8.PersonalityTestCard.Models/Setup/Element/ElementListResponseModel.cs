a3b9f180cdce28b1c1a1c9449fa15a77fdf49135
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 21:22:12 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/Element/ElementListResponseModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/Element/ElementListResponseModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Models.Setup.Element;
 
+#region ElementListResponseModel
+
 public class ElementListResponseModel
 {
 	public List<ElementListModel> DataLst { get; set; }
 }
+
+#endregion
