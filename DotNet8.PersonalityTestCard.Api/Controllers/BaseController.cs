b4d54d7b07ace72948767078b68ef03e350234a3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 10:48:16 +0630

    Feat : Add Content and InternalServerError in Base Controller

--- DotNet8.PersonalityTestCard.Api/Controllers/BaseController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DotNet8.PersonalityTestCard.Api.Controllers
 {
@@ -7,5 +8,16 @@ namespace DotNet8.PersonalityTestCard.Api.Controllers
 	[ApiController]
 	public class BaseController : ControllerBase
 	{
+
+		protected IActionResult Content(object? obj)
+		{
+			return Content(JsonConvert.SerializeObject(obj), "application/json"); 
+		}
+
+		protected IActionResult InternalServerError(Exception ex)
+		{
+			return StatusCode(500, ex.Message);
+		}
+
 	}
 }
