1ad96ca4da19e4e3efb935d46b5d698da8fdd0a6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 23:54:11 +0630

    Feat : Add ElementController

--- DotNet8.PersonalityTestCard.Api/Controllers/Element/ElementController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/Element/ElementController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet8.PersonalityTestCard.Api.Features.Element.Queries.GetElementList;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.PersonalityTestCard.Api.Controllers.Element
 {
 	[Route("api/[controller]")]
 	[ApiController]
-	public class ElementController : ControllerBase
+	public class ElementController : BaseController
 	{
+		private readonly IMediator _mediator;
+
+		public ElementController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetElementAsync()
+		{
+			try
+			{
+				var query = new GetElementListQuery();
+				var lst = await _mediator.Send(query);
+
+				return Content(lst);
+			}
+
+			catch (Exception ex)
+			{
+				return InternalServerError(ex);
+			}
+		}
 	}
 }
