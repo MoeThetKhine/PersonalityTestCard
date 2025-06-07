31f3e3977363ea663968ae652be64beec19412e6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:40:29 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Repositories/Card/ICardRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Card/ICardRepository.cs
@@ -1,6 +1,4 @@
-﻿using DotNet8.PersonalityTestCard.Models.Setup.Card;
-
-namespace DotNet8.PersonalityTestCard.Api.Repositories.Card
+﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.Card
 {
 	public interface ICardRepository
 	{
