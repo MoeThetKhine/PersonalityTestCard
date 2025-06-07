9f85a09d2599bb17b10936fbf74e21b5caebbc0b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:36:03 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/Card/Queries/GetCardList/GetCardListQuery.cs
+++ DotNet8.PersonalityTestCard.Api/Features/Card/Queries/GetCardList/GetCardListQuery.cs
@@ -1,7 +1,4 @@
-﻿using DotNet8.PersonalityTestCard.Models.Setup.Card;
-using MediatR;
-
-namespace DotNet8.PersonalityTestCard.Api.Features.Card.Queries.GetCardList;
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.Card.Queries.GetCardList;
 
 public class GetCardListQuery : IRequest<CardListResponseModel>
 {
