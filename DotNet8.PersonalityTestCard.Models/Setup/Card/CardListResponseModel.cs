55012c0f2164bf44f5cb355e15f3ef13ff40f5c6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:27:37 +0630

    Feat : Create CardListResponseModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/Card/CardListResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.Card
+{
+	public class CardListResponseModel
+	{
+		public List<CardListModel> DataLst { get; set; }
+	}
+}
