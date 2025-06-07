855e7693147039dea65f7450c42048ed6e6293ab
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 23:53:56 +0630

    Feat : Add GetElementListQueryHandler

--- DotNet8.PersonalityTestCard.Api/Features/Element/Queries/GetElementList/GetElementListQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/Element/Queries/GetElementList/GetElementListQueryHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Features.Element.Queries.GetElementList
+﻿using DotNet8.PersonalityTestCard.Api.Repositories.Element;
+
+namespace DotNet8.PersonalityTestCard.Api.Features.Element.Queries.GetElementList
 {
-	public class GetElementListQueryHandler
+	public class GetElementListQueryHandler : IRequestHandler<GetElementListQuery, ElementListResponseModel>
 	{
+		private readonly IElementRepository _elementRepository;
+
+		public GetElementListQueryHandler(IElementRepository elementRepository)
+		{
+			_elementRepository = elementRepository;
+		}
+
+		public async Task<ElementListResponseModel> Handle(GetElementListQuery request, CancellationToken cancellationToken)
+		{
+			return await _elementRepository.GetElementAsync();
+		}
 	}
 }
