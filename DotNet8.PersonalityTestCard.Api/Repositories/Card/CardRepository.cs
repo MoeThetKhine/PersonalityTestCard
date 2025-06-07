b0350ae5b3e047fccaa71db6b6c6a8022fe680e8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:39:36 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Repositories/Card/CardRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Card/CardRepository.cs
@@ -1,42 +1,41 @@
 ﻿using DotNet8.PersonalityTestCard.Models;
 
-namespace DotNet8.PersonalityTestCard.Api.Repositories.Blog
+namespace DotNet8.PersonalityTestCard.Api.Repositories.Card;
+
+public class CardRepository : ICardRepository
 {
-	public class CardRepository : ICardRepository
+
+	private readonly AppDbContext _appDbContext;
+
+	public CardRepository(AppDbContext appDbContext)
 	{
-
-		private readonly AppDbContext _appDbContext;
-
-		public CardRepository(AppDbContext appDbContext)
-		{
-			_appDbContext = appDbContext;
-		}
-
-		#region GetCardsAsync
-
-		public async Task<CardListResponseModel> GetCardsAsync()
-		{
-			try
-			{
-				var dataLst = await _appDbContext.TblCards
-					 .AsNoTracking()
-					 .OrderBy(x => x.CardName)
-					 .ToListAsync();
-
-				var lst = dataLst.Select(x => x.Change()).ToList();
-
-				CardListResponseModel responseModel = new()
-				{
-					DataLst = lst
-				};
-				return responseModel;
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(ex.Message);
-			}
-		}
-
-		#endregion
+		_appDbContext = appDbContext;
 	}
+
+	#region GetCardsAsync
+
+	public async Task<CardListResponseModel> GetCardsAsync()
+	{
+		try
+		{
+			var dataLst = await _appDbContext.TblCards
+				 .AsNoTracking()
+				 .OrderBy(x => x.CardName)
+				 .ToListAsync();
+
+			var lst = dataLst.Select(x => x.Change()).ToList();
+
+			CardListResponseModel responseModel = new()
+			{
+				DataLst = lst
+			};
+			return responseModel;
+		}
+		catch (Exception ex)
+		{
+			throw new Exception(ex.Message);
+		}
+	}
+
+	#endregion
 }
