533711fa2932d5df56dee5230a8e04779c88fdbf
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:40:57 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/Card/CardListResponseModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/Card/CardListResponseModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.PersonalityTestCard.Models.Setup.Card
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.Card
 {
 	public class CardListResponseModel
 	{
