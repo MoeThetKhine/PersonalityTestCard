204244d98bb4e5d72d81deff7932f46f7cf24357
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 21:34:59 +0630

    Feat : Add IElementRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/Element/IElementRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Element/IElementRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.Element
+﻿using DotNet8.PersonalityTestCard.Models.Setup.Element;
+
+namespace DotNet8.PersonalityTestCard.Api.Repositories.Element
 {
 	public interface IElementRepository
 	{
+		Task<ElementListResponseModel> GetCardsAsync();
 	}
 }
