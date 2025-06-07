975c928040fe9a10c4a5662b6769c2e5755a53e2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 19:02:19 +0630

    -

--- DotNet8.PersonalityTestCard.Api/ModularService.cs
+++ DotNet8.PersonalityTestCard.Api/ModularService.cs
@@ -27,6 +27,8 @@ public static class ModularService
 
 	#endregion
 
+	#region AddDbContextService
+
 	private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
 	{
 		builder.Services.AddDbContext<AppDbContext>(opt =>
@@ -36,6 +38,8 @@ public static class ModularService
 
 		return services;
 	}
+
+	#endregion
 
 	private static IServiceCollection AddMediatRService(this IServiceCollection services)
 	{
