c71a919fa5ecf0bf116a656fc8236c28d4f7f478
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-07 20:26:58 +0630

    Feat : Update ChangeModel

--- DotNet8.PersonalityTestCard.Models/ChangeModel.cs
+++ DotNet8.PersonalityTestCard.Models/ChangeModel.cs
@@ -1,20 +1,24 @@
-﻿namespace DotNet8.PersonalityTestCard.Models;
+﻿using DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
+
+namespace DotNet8.PersonalityTestCard.Models;
 
 public static class ChangeModel
 {
 
 	#region CardListModel
 
-	public static CardListModel Change(this CardListModel dataModel)
+	public static CardListModel Change(this TblCard dataModel)
 	{
 		return new CardListModel
 		{
 			CardId = dataModel.CardId,
 			CardName = dataModel.CardName,
-			ElementId = dataModel.ElementId,
+			//ElementId = dataModel.ElementId,
 		};
 	}
 
 	#endregion
+
+
 
 }
