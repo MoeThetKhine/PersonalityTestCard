438c7fb00efbcfedd797839d3d4771ed6be69c92
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 11:50:44 +0630

    Feat : Add GetUserListQueryHandler

--- DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserList/GetUserListQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserList/GetUserListQueryHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserList
+﻿using DotNet8.PersonalityTestCard.Api.Repositories.User;
+using DotNet8.PersonalityTestCard.Models.Setup.User;
+
+namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserList
 {
-	public class GetUserListQueryHandler
+	public class GetUserListQueryHandler : IRequestHandler<GetUserListQuery, UserListResponseModel>
 	{
+		private readonly IUserRepository _userRepository;
+
+		public GetUserListQueryHandler(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+		public async Task<UserListResponseModel> Handle(GetUserListQuery request, CancellationToken cancellationToken)
+		{
+			return await _userRepository.GetUserListAsync();
+		}
 	}
 }
