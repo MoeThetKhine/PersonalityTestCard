b2e24d50c6a6a342917bc2c6a2000f5ae5e41015
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-09 00:35:07 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Features/UserCard/Command/CreateUserCard/CreateUserCardCommand.cs
+++ DotNet8.PersonalityTestCard.Api/Features/UserCard/Command/CreateUserCard/CreateUserCardCommand.cs
@@ -1,8 +1,7 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserCard.Command.CreateUserCard
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserCard.Command.CreateUserCard;
+
+public class CreateUserCardCommand : IRequest<int>
 {
-	public class CreateUserCardCommand : IRequest<int>
-	{
-		public int UserId { get; set; }
-		public List<int> CardIds { get; set; }
-	}
+	public int UserId { get; set; }
+	public List<int> CardIds { get; set; }
 }
