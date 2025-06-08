83f89d6a5cfd8e0c4dc1786376c56d9363047a5e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:08:21 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Repositories/User/UserRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/User/UserRepository.cs
@@ -1,42 +1,41 @@
 ﻿using DotNet8.PersonalityTestCard.Models.Setup.User;
 
-namespace DotNet8.PersonalityTestCard.Api.Repositories.User
+namespace DotNet8.PersonalityTestCard.Api.Repositories.User;
+
+public class UserRepository : IUserRepository
 {
-	public class UserRepository : IUserRepository
+	private readonly AppDbContext _context;
+
+	public UserRepository(AppDbContext context)
 	{
-		private readonly AppDbContext _context;
-
-		public UserRepository(AppDbContext context)
-		{
-			_context = context;
-		}
-
-		#region GetUserListAsync
-
-		public async Task<UserListResponseModel> GetUserListAsync()
-		{
-			try
-			{
-				var dataLst = await _context.TblUsers
-					.AsNoTracking()
-					.OrderByDescending(x => x.UserId)
-					.ToListAsync();
-
-				var lst = dataLst.Select(x => x.Change()).ToList();
-
-				UserListResponseModel responseModel = new()
-				{
-					DataLst = lst
-				};
-
-				return responseModel;
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(ex.Message);
-			}
-		}
-
-		#endregion
+		_context = context;
 	}
+
+	#region GetUserListAsync
+
+	public async Task<UserListResponseModel> GetUserListAsync()
+	{
+		try
+		{
+			var dataLst = await _context.TblUsers
+				.AsNoTracking()
+				.OrderByDescending(x => x.UserId)
+				.ToListAsync();
+
+			var lst = dataLst.Select(x => x.Change()).ToList();
+
+			UserListResponseModel responseModel = new()
+			{
+				DataLst = lst
+			};
+
+			return responseModel;
+		}
+		catch (Exception ex)
+		{
+			throw new Exception(ex.Message);
+		}
+	}
+
+	#endregion
 }
