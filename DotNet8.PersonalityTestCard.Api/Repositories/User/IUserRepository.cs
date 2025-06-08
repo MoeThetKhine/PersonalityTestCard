558eb6930cedfd18e8ddd2b8e07c9df30897fe4d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 11:49:21 +0630

    Feat : Add GetUserListAsync in IUserRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/User/IUserRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/User/IUserRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.User
+﻿using DotNet8.PersonalityTestCard.Models.Setup.User;
+
+namespace DotNet8.PersonalityTestCard.Api.Repositories.User
 {
 	public interface IUserRepository
 	{
+		Task<UserListResponseModel> GetUserListAsync();
 	}
 }
