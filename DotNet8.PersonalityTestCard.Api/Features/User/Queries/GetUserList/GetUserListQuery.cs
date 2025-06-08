3c9f3e31ebcb634055c158816580828c68cd9a99
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:09:25 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserList/GetUserListQuery.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserList/GetUserListQuery.cs
@@ -1,6 +1,4 @@
-﻿using DotNet8.PersonalityTestCard.Models.Setup.User;
-
-namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserList
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserList
 {
 	public class GetUserListQuery : IRequest<UserListResponseModel>
 	{
