838b3f5261967fe1bcacd373a26d072d6d07ba8e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-09 00:18:25 +0630

    Feat : Add UserCardRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/UserCard/UserCardRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/UserCard/UserCardRepository.cs
@@ -1,5 +1,34 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.UserCard;
 
-public class UserCardRepository
+public class UserCardRepository : IUserCardRepository
 {
+	private readonly AppDbContext _context;
+
+	public UserCardRepository(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<List<TblUserCard>> GetUserCardsAsync(int userId)
+	{
+		return await _context.TblUserCards
+			.Where(x => x.UserId == userId)
+			.ToListAsync();
+	}
+
+	public async Task RemoveUserCardsAsync(List<TblUserCard> userCards)
+	{
+		_context.TblUserCards.RemoveRange(userCards);
+		await Task.CompletedTask;
+	}
+
+	public async Task AddUserCardsAsync(List<TblUserCard> userCards)
+	{
+		await _context.TblUserCards.AddRangeAsync(userCards);
+	}
+
+	public async Task SaveChangesAsync()
+	{
+		await _context.SaveChangesAsync();
+	}
 }
