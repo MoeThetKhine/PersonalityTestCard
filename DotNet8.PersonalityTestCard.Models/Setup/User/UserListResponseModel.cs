89364438ce59766f8588d74f8a9aa0ce6a80d5fa
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:18:04 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Models/Setup/User/UserListResponseModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/User/UserListResponseModel.cs
@@ -1,7 +1,6 @@
-﻿namespace DotNet8.PersonalityTestCard.Models.Setup.User
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.User;
+
+public class UserListResponseModel
 {
-	public class UserListResponseModel
-	{
-		public List<UserModel> DataLst {  get; set; }
-	}
+	public List<UserModel> DataLst {  get; set; }
 }
