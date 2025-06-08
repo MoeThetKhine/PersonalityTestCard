5cdf971d4597a4d125cad9bf8c6eb5fcad0a68e6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 14:10:12 +0630

    Feat : Add UserRequestModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/User/UserRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.User
+{
+	public class UserRequestModel
+	{
+		public string Username { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+	}
+}
