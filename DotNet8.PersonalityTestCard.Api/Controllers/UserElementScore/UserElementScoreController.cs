66268643fdd25d02cd8a4ddd72db90d67caac222
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:21:03 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Controllers/UserElementScore/UserElementScoreController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/UserElementScore/UserElementScoreController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace DotNet8.PersonalityTestCard.Api.Controllers.UserElementScore
+﻿namespace DotNet8.PersonalityTestCard.Api.Controllers.UserElementScore
 {
 	[Route("api/[controller]")]
 	[ApiController]
