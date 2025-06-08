50a69962098fb19d63de364453130c07fc2b3407
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-09 00:34:44 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardRequestModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardRequestModel.cs
@@ -1,9 +1,12 @@
 ﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard;
 
+#region UserCardRequestModel
+
 public class UserCardRequestModel
 {
-	
 	public int UserId { get; set; }
 	public List<int> CardIds { get; set; }
 
 }
+
+#endregion
