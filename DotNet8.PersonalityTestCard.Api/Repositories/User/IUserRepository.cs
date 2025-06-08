0696a353e8f42422ba2ad0d89fa14456836a2e27
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 17:00:41 +0630

    Feat : Add UpdateUserAsync in IUserRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/User/IUserRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/User/IUserRepository.cs
@@ -5,4 +5,5 @@ public interface IUserRepository
 	Task<UserListResponseModel> GetUserListAsync();
 	Task<UserModel> GetUserByIdAsync(int id);
 	Task<int> CreateUserAsync(UserRequestModel requestModel);
+	Task<int> UpdateUserAsync(UserRequestModel requestModel, int id);
 }
