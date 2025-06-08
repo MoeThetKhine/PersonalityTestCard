4b1be6e89d15403a634260f1e04edb80828a365e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:18:22 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/User/UserListResponseModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/User/UserListResponseModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Models.Setup.User;
 
+#region UserListResponseModel
+
 public class UserListResponseModel
 {
 	public List<UserModel> DataLst {  get; set; }
 }
+
+#endregion
