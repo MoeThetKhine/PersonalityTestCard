da116d012d0906872f37feb1cd9ec4ce6f18fc83
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 15:15:56 +0630

    Feat : Add GetUserByIdQueryHandler

--- DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -1,5 +1,4 @@
-﻿
-namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserById;
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserById;
 
 public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserModel>
 {
