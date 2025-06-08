cc62bac1071d591dad660ef1d282f013b718bdbe
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:28:01 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Repositories/Element/ElementRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Element/ElementRepository.cs
@@ -1,38 +1,36 @@
-﻿
-namespace DotNet8.PersonalityTestCard.Api.Repositories.Element
+﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.Element;
+
+public class ElementRepository : IElementRepository
 {
-	public class ElementRepository : IElementRepository
+	private readonly AppDbContext _appDbContext;
+
+	public ElementRepository(AppDbContext appDbContext)
 	{
-		private readonly AppDbContext _appDbContext;
+		_appDbContext = appDbContext;
+	}
 
-		public ElementRepository(AppDbContext appDbContext)
+	public async Task<ElementListResponseModel> GetElementAsync()
+	{
+		try
 		{
-			_appDbContext = appDbContext;
+			var dataLst = await _appDbContext.TblElements
+				.AsNoTracking()
+				.OrderByDescending(x => x.ElementId)
+				.ToListAsync();
+
+			var lst = dataLst.Select(x => x.Change()).ToList();
+
+			ElementListResponseModel responseModel = new()
+			{
+				DataLst = lst
+			};
+
+			return responseModel;
+		}
+		catch (Exception ex)
+		{
+			throw new Exception(ex.Message);
 		}
 
-		public async Task<ElementListResponseModel> GetElementAsync()
-		{
-			try
-			{
-				var dataLst = await _appDbContext.TblElements
-					.AsNoTracking()
-					.OrderByDescending(x => x.ElementId)
-					.ToListAsync();
-
-				var lst = dataLst.Select(x => x.Change()).ToList();
-
-				ElementListResponseModel responseModel = new()
-				{
-					DataLst = lst
-				};
-
-				return responseModel;
-			}
-			catch (Exception ex)
-			{
-				throw new Exception(ex.Message);
-			}
-
-		}
 	}
 }
