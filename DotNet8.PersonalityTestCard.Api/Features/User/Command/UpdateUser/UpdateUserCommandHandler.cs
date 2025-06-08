a530d0f11b020341daa493c538d51e70a5ed7aac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 17:01:55 +0630

    Feat : Add UpdateUserCommandHandler

--- DotNet8.PersonalityTestCard.Api/Features/User/Command/UpdateUser/UpdateUserCommandHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Command/UpdateUser/UpdateUserCommandHandler.cs
@@ -1,5 +1,21 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Command.UpdateUser;
 
-public class UpdateUserCommandHandler
+public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand , int>
 {
+	private readonly IUserRepository _userRepository;
+
+	public UpdateUserCommandHandler(IUserRepository userRepository)
+	{
+		this._userRepository = userRepository;
+	}
+
+	public async Task<int> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+	{
+		if(request.UserId <= 0)
+		{
+			throw new Exception("Blog Id cannot be empty.");
+		}
+
+		return await _userRepository.UpdateUserAsync(request.userRequestModel, request.UserId);
+	}
 }
