c2f12de565871bfd43bf982819d99816b7f97742
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-09 00:20:24 +0630

    Feat : Add UserCardRequestModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard
+{
+	public class UserCardRequestModel
+	{
+		
+		public int UserId { get; set; }
+		public List<int> CardIds { get; set; }
+
+	}
+}
