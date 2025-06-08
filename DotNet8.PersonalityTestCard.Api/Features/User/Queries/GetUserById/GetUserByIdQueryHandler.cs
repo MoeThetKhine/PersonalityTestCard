ed1dd448b93000ec37edf8df33c1d1e7aace1608
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 15:02:22 +0630

    Feat : Add GetUserByIdQueryHandler

--- DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -1,5 +1,21 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserById;
+﻿
+namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserById;
 
-public class GetUserByIdQueryHandler
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserModel>
 {
+
+	private readonly IUserRepository _userRepository;
+
+	public GetUserByIdQueryHandler(IUserRepository userRepository)
+	{
+		_userRepository = userRepository;
+	}
+
+	public async Task<UserModel> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+	{
+		if (request.UserId <= 0)
+			throw new Exception("Id cannot be empty");
+
+		return await _userRepository.GetUserByIdAsync(request.UserId);
+	}
 }
