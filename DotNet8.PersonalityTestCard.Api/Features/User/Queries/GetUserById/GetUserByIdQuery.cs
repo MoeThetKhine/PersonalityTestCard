ca047fabbdd0844ef6c493d3c49da236bd5dea44
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 15:02:02 +0630

    Feat : Add GetUserByIdQuery

--- DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQuery.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQuery.cs
@@ -1,5 +1,6 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserById;
 
-public class GetUserByIdQuery
+public class GetUserByIdQuery : IRequest<UserModel>
 {
+	public int UserId {  get; set; }
 }
