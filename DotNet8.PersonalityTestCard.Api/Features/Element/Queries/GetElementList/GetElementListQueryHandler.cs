905891fbb59ac7fcc5b056fadea15885176bc97c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 13:22:14 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Features/Element/Queries/GetElementList/GetElementListQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/Element/Queries/GetElementList/GetElementListQueryHandler.cs
@@ -1,19 +1,18 @@
 ﻿using DotNet8.PersonalityTestCard.Api.Repositories.Element;
 
-namespace DotNet8.PersonalityTestCard.Api.Features.Element.Queries.GetElementList
+namespace DotNet8.PersonalityTestCard.Api.Features.Element.Queries.GetElementList;
+
+public class GetElementListQueryHandler : IRequestHandler<GetElementListQuery, ElementListResponseModel>
 {
-	public class GetElementListQueryHandler : IRequestHandler<GetElementListQuery, ElementListResponseModel>
+	private readonly IElementRepository _elementRepository;
+
+	public GetElementListQueryHandler(IElementRepository elementRepository)
 	{
-		private readonly IElementRepository _elementRepository;
+		_elementRepository = elementRepository;
+	}
 
-		public GetElementListQueryHandler(IElementRepository elementRepository)
-		{
-			_elementRepository = elementRepository;
-		}
-
-		public async Task<ElementListResponseModel> Handle(GetElementListQuery request, CancellationToken cancellationToken)
-		{
-			return await _elementRepository.GetElementAsync();
-		}
+	public async Task<ElementListResponseModel> Handle(GetElementListQuery request, CancellationToken cancellationToken)
+	{
+		return await _elementRepository.GetElementAsync();
 	}
 }
