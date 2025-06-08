eff0303a8a9d645e2294c1da2e16f34fa862e83d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 11:49:49 +0630

    Feat : Add GetUserListAsync in UserRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/User/UserRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/User/UserRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.User
+﻿using DotNet8.PersonalityTestCard.Models.Setup.User;
+
+namespace DotNet8.PersonalityTestCard.Api.Repositories.User
 {
-	public class UserRepository
+	public class UserRepository : IUserRepository
 	{
+		private readonly AppDbContext _context;
+
+		public UserRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<UserListResponseModel> GetUserListAsync()
+		{
+			try
+			{
+				var dataLst = await _context.TblUsers
+					.AsNoTracking()
+					.OrderByDescending(x => x.UserId)
+					.ToListAsync();
+
+				var lst = dataLst.Select(x => x.Change()).ToList();
+
+				UserListResponseModel responseModel = new()
+				{
+					DataLst = lst
+				};
+
+				return responseModel;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
 	}
 }
