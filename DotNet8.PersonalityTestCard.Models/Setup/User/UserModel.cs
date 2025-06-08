6aafb61b390164820773a1ccf3ceeb8918ae8806
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 11:17:19 +0630

    Feat : Add UserModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/User/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.User
+{
+	public class UserModel
+	{
+		public int UserId { get; set; }
+
+		public string Username { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public DateTime CreatedAt { get; set; }
+	}
+}
