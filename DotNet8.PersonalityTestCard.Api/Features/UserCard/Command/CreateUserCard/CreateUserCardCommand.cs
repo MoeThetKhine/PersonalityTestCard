70759d80983a095d77f305679cf8085a90e478b1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-09 00:19:01 +0630

    Feat : Add CreateUserCardCommand

---
+++ DotNet8.PersonalityTestCard.Api/Features/UserCard/Command/CreateUserCard/CreateUserCardCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserCard.Command.CreateUserCard
+{
+	public class CreateUserCardCommand : IRequest<int>
+	{
+		public int UserId { get; set; }
+		public List<int> CardIds { get; set; }
+	}
+}
