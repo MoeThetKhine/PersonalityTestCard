4e50f0cbcb84cd585d6f961132f8f8c78819d2df
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 11:53:31 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Controllers/User/UserController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/User/UserController.cs
@@ -1,32 +1,31 @@
 ﻿using DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserList;
 
-namespace DotNet8.PersonalityTestCard.Api.Controllers.User
+namespace DotNet8.PersonalityTestCard.Api.Controllers.User;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : BaseController
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class UserController : BaseController
+	private readonly IMediator _mediator;
+
+	public UserController(IMediator mediator)
 	{
-		private readonly IMediator _mediator;
+		_mediator = mediator;
+	}
 
-		public UserController(IMediator mediator)
+	[HttpGet]
+	public async Task<IActionResult> GetUserListAsync()
+	{
+		try
 		{
-			_mediator = mediator;
+			var query = new GetUserListQuery();
+			var lst = await _mediator.Send(query);
+
+			return Content(lst);
 		}
-
-		[HttpGet]
-		public async Task<IActionResult> GetUserListAsync()
+		catch (Exception ex)
 		{
-			try
-			{
-				var query = new GetUserListQuery();
-				var lst = await _mediator.Send(query);
-
-				return Content(lst);
-			}
-			catch (Exception ex)
-			{
-				return InternalServerError(ex);
-			}
+			return InternalServerError(ex);
 		}
 	}
 }
