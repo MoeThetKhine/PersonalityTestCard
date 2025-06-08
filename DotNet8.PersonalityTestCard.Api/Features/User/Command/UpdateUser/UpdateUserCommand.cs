5f7719f915136dd05c1022ee0611e7f7b3248198
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 17:01:24 +0630

    Feat : Add UpdateUserCommand

--- DotNet8.PersonalityTestCard.Api/Features/User/Command/UpdateUser/UpdateUserCommand.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Command/UpdateUser/UpdateUserCommand.cs
@@ -1,5 +1,7 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Command.UpdateUser;
 
-public class UpdateUserCommand
+public class UpdateUserCommand : IRequest<int>
 {
+	public UserRequestModel userRequestModel { get; set; }
+	public int UserId {  get; set; }
 }
