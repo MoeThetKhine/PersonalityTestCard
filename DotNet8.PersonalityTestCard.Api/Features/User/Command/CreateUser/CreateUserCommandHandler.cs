7602bfd6bdc49fe5e2380ba675c11e486882ebb6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 14:36:37 +0630

    Feat : Add CreateUserCommandHandler

--- DotNet8.PersonalityTestCard.Api/Features/User/Command/CreateUser/CreateUserCommandHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Command/CreateUser/CreateUserCommandHandler.cs
@@ -1,5 +1,25 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Command.CreateUser;
 
-public class CreateUserCommandHandler
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
 {
+	private readonly IUserRepository _userRepository;
+
+	public CreateUserCommandHandler(IUserRepository userRepository)
+	{
+		_userRepository = userRepository;
+	}
+
+	public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+	{
+		if(string.IsNullOrEmpty(request.userRequest.Username))
+		{
+			throw new Exception("Username cannot be empty");
+		}
+		if (string.IsNullOrEmpty(request.userRequest.Email))
+		{
+			throw new Exception("Email cannot be empty");
+		}
+
+		return await _userRepository.CreateUserAsync(request.userRequest);
+	}
 }
