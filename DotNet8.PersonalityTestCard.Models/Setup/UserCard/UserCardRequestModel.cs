81b7cc770c3e4acfbba991b42dff6cb8ec8f642d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-09 00:34:15 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardRequestModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardRequestModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard
-{
-	public class UserCardRequestModel
-	{
-		
-		public int UserId { get; set; }
-		public List<int> CardIds { get; set; }
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard;
 
-	}
+public class UserCardRequestModel
+{
+	
+	public int UserId { get; set; }
+	public List<int> CardIds { get; set; }
+
 }
