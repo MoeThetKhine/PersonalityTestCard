9d770e9ccd88ffdfeecf7e16f14808430c2de113
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-08 21:49:47 +0630

    Feat : Add User Card Model

---
+++ DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard
+{
+	public class UserCardModel
+	{
+		public int UserCardId { get; set; }
+
+		public int UserId { get; set; }
+
+		public int CardId { get; set; }
+	}
+}
