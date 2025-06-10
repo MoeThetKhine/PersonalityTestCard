a28f0c08bf438c6ded0f9f3ac426360ebdaf6f88
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:43:44 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Repositories/Card/ICardRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Card/ICardRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.Card;
 
+#region ICardRepository
+
 public interface ICardRepository
 {
 	Task<CardListResponseModel> GetCardsAsync();
 }
+
+#endregion
