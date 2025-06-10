92fae6f4710f733d978d889ef7274ee33716aad3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-11 00:13:02 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreResponseModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreResponseModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserElementScore
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserElementScore;
+
+public class UserElementScoreResponseModel
 {
-	public class UserElementScoreResponseModel
-	{
-		public int UserId { get; set; }
-		public string Username {  get; set; }
-		public string ElementName { get; set; }
-		public int Score { get; set; }
-	}
+	public int UserId { get; set; }
+	public string Username {  get; set; }
+	public string ElementName { get; set; }
+	public int Score { get; set; }
 }
