28478b78f8c3f3ae8cfd18df00116202fd689d88
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:31:11 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreRequestModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreRequestModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.PersonalityTestCard.Models.Setup.UserElementScore
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserElementScore
 {
 	public class UserElementScoreRequestModel
 	{
