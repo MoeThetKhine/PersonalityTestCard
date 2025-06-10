157c7c91e6fc4bb50161192835b5a102b85b5c27
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-11 00:08:59 +0630

    Feat : Add UserElementScoreResponseModel

---
+++ DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.PersonalityTestCard.Models.Setup.UserElementScore
+{
+	public class UserElementScoreResponseModel
+	{
+		public int UserId { get; set; }
+		public string Username {  get; set; }
+		public string ElementName { get; set; }
+		public int Score { get; set; }
+	}
+}
