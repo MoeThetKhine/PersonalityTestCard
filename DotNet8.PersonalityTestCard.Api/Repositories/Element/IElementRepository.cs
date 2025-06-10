05fc390e0f06fbd48e5c28180f5155032dfeb2bd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:44:14 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Repositories/Element/IElementRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/Element/IElementRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.Element;
 
+#region IElementRepository
+
 public interface IElementRepository
 {
 	Task<ElementListResponseModel> GetElementAsync();
 }
+
+#endregion
