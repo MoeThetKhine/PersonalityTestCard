a71d47d6f9697e6fb0eb4120ab7159201966d922
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:28:55 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/UserCard/UserCardModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard
+﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserCard
 {
 	public class UserCardModel
 	{
