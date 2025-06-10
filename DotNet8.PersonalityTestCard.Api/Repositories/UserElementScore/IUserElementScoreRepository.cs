d09e92a0995ca9ce0e187b394553e28d2d9c11a2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:19:11 +0630

    Feat : Add IUserElementScoreRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/UserElementScore/IUserElementScoreRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/UserElementScore/IUserElementScoreRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.UserElementScore;
+﻿using DotNet8.PersonalityTestCard.Models.Setup.UserElementScore;
+
+namespace DotNet8.PersonalityTestCard.Api.Repositories.UserElementScore;
 
 public interface IUserElementScoreRepository
 {
+	Task<UserElementScoreRequestModel> GetUserElementScoreAsync();
 }
