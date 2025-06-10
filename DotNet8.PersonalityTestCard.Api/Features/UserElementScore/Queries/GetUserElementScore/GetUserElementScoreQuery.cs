ee84cb5695ade3f7c6e0b748d81670aad71f08ab
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:19:48 +0630

    Feat : Add GetUserElementScoreQuery

---
+++ DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScore/GetUserElementScoreQuery.cs
@@ -0,0 +1,8 @@
+﻿using DotNet8.PersonalityTestCard.Models.Setup.UserElementScore;
+
+namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScore
+{
+	public class GetUserElementScoreQuery : IRequest<UserElementScoreRequestModel>
+	{
+	}
+}
