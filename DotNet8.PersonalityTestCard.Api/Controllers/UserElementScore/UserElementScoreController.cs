856bb6853d3c1c2774453bad555239d923ab0c0e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:22:01 +0630

    Feat : Add GetUserElementScoreAsync

--- DotNet8.PersonalityTestCard.Api/Controllers/UserElementScore/UserElementScoreController.cs
+++ DotNet8.PersonalityTestCard.Api/Controllers/UserElementScore/UserElementScoreController.cs
@@ -1,7 +1,31 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Controllers.UserElementScore;
+﻿using DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScore;
+
+namespace DotNet8.PersonalityTestCard.Api.Controllers.UserElementScore;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UserElementScoreController : ControllerBase
+public class UserElementScoreController : BaseController
 {
+	private readonly IMediator _mediator;
+
+	public UserElementScoreController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetUserElementScoreAsync()
+	{
+		try
+		{
+			var query = new GetUserElementScoreQuery();
+			var lst = await _mediator.Send(query);
+
+			return Content(lst);
+		}
+		catch (Exception ex)
+		{
+			return InternalServerError(ex);
+		}
+	}
 }
