8d4eeb7eb92b9639155e6c4c0bd516eee079c316
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:41:21 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScore/GetUserElementScoreQuery.cs
+++ DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScore/GetUserElementScoreQuery.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScore
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScore;
+
+public class GetUserElementScoreQuery : IRequest<UserElementScoreRequestModel>
 {
-	public class GetUserElementScoreQuery : IRequest<UserElementScoreRequestModel>
-	{
-	}
 }
