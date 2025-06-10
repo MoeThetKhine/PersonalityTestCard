417cb02f8bd11ad4744ec9dd0cbe689cea90062d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-11 00:08:38 +0630

    Feat : Add GetUserElementScoreByUserIdQueryHandler

---
+++ DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScoreByUserId/GetUserElementScoreByUserIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScoreByUserId
+{
+	public class GetUserElementScoreByUserIdQueryHandler : IRequestHandler<GetUserElementScoreByUserIdQuery, UserElementScoreResponseModel>
+	{
+		private readonly IUserElementScoreRepository _userElementScoreRepository;
+
+		public GetUserElementScoreByUserIdQueryHandler(IUserElementScoreRepository userElementScoreRepository)
+		{
+			_userElementScoreRepository = userElementScoreRepository;
+		}
+
+		public async Task<UserElementScoreResponseModel> Handle(GetUserElementScoreByUserIdQuery request, CancellationToken cancellationToken)
+		{
+			if (request.UserId <= 0)
+				throw new Exception("User Id cannot be empty");
+
+			return await _userElementScoreRepository.GetUserElementScoreByUserIdAsync(request.UserId);
+		}
+	}
+}
