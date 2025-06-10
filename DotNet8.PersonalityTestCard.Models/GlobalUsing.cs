5cfd4b808fc0df98a944aa794250c06e1dfe135f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:32:38 +0630

    -

--- DotNet8.PersonalityTestCard.Models/GlobalUsing.cs
+++ DotNet8.PersonalityTestCard.Models/GlobalUsing.cs
@@ -3,5 +3,3 @@ global using DotNet8.PersonalityTestCard.DbService.AppDbContextModels;
 global using DotNet8.PersonalityTestCard.Models.Setup.Element;
 global using DotNet8.PersonalityTestCard.Models.Setup.User;
 global using DotNet8.PersonalityTestCard.Models.Setup.UserElementScore;
-
-
