3c84e5b66cd1003e5bb9717b03a4432d526e0766
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:31:39 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreRequestModel.cs
+++ DotNet8.PersonalityTestCard.Models/Setup/UserElementScore/UserElementScoreRequestModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Models.Setup.UserElementScore;
 
+#region UserElementScoreRequestModel
+
 public class UserElementScoreRequestModel
 {
 	public List<UserElementScoreModel> DataLst { get; set; }
 }
+
+#endregion
