cc05bbc4d78667f52eded11dd621012d1d3ba969
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-10 10:19:32 +0630

    Feat : Add UserElementScoreRepository

--- DotNet8.PersonalityTestCard.Api/Repositories/UserElementScore/UserElementScoreRepository.cs
+++ DotNet8.PersonalityTestCard.Api/Repositories/UserElementScore/UserElementScoreRepository.cs
@@ -1,5 +1,39 @@
-﻿namespace DotNet8.PersonalityTestCard.Api.Repositories.UserElementScore;
+﻿using DotNet8.PersonalityTestCard.Models.Setup.UserElementScore;
 
-public class UserElementScoreRepository
+namespace DotNet8.PersonalityTestCard.Api.Repositories.UserElementScore;
+
+public class UserElementScoreRepository : IUserElementScoreRepository
 {
+	private readonly AppDbContext _appDbContext;
+
+	public UserElementScoreRepository(AppDbContext appDbContext)
+	{
+		_appDbContext = appDbContext;
+	}
+
+	public async Task<UserElementScoreRequestModel> GetUserElementScoreAsync()
+	{
+		try
+		{
+			var dataLst = await _appDbContext.TblUserElementScores
+					.Include(x => x.Element)
+					.AsNoTracking()
+					.OrderByDescending(x => x.UserElementId)
+					.ToListAsync();
+
+
+			var lst = dataLst.Select(x => x.Change()).ToList();
+
+			UserElementScoreRequestModel responseModel = new()
+			{
+				DataLst = lst
+			};
+
+			return responseModel;
+		}
+		catch (Exception ex)
+		{
+			throw new Exception(ex.Message);
+		}
+	}
 }
