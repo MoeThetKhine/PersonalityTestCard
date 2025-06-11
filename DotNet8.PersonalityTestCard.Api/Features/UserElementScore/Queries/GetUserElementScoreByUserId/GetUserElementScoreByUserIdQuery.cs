97762adf0ff3375d537924530888e650eb215da1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-11 09:21:35 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScoreByUserId/GetUserElementScoreByUserIdQuery.cs
+++ DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScoreByUserId/GetUserElementScoreByUserIdQuery.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScoreByUserId;
 
+#region GetUserElementScoreByUserIdQuery
+
 public class GetUserElementScoreByUserIdQuery : IRequest<UserElementScoreResponseModel>
 {
 	public int UserId {  get; set; }
 }
+
+#endregion
