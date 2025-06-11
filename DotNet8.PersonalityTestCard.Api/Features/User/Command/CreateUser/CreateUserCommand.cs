9e8b44c4d212439ee18249407e8c7eb4d79560c1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-11 09:25:50 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/User/Command/CreateUser/CreateUserCommand.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Command/CreateUser/CreateUserCommand.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Command.CreateUser;
 
+#region CreateUserCommand
+
 public class CreateUserCommand : IRequest<int>
 {
 	public UserRequestModel userRequest {  get; set; }
 }
+
+#endregion
