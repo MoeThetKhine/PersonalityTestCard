ac7f0e47c7db15089747c4364d48b581d2be18d2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-11 09:16:15 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScoreByUserId/GetUserElementScoreByUserIdQueryHandler.cs
+++ DotNet8.PersonalityTestCard.Api/Features/UserElementScore/Queries/GetUserElementScoreByUserId/GetUserElementScoreByUserIdQueryHandler.cs
@@ -1,21 +1,19 @@
-﻿
-namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScoreByUserId
+﻿namespace DotNet8.PersonalityTestCard.Api.Features.UserElementScore.Queries.GetUserElementScoreByUserId;
+
+public class GetUserElementScoreByUserIdQueryHandler : IRequestHandler<GetUserElementScoreByUserIdQuery, UserElementScoreResponseModel>
 {
-	public class GetUserElementScoreByUserIdQueryHandler : IRequestHandler<GetUserElementScoreByUserIdQuery, UserElementScoreResponseModel>
+	private readonly IUserElementScoreRepository _userElementScoreRepository;
+
+	public GetUserElementScoreByUserIdQueryHandler(IUserElementScoreRepository userElementScoreRepository)
 	{
-		private readonly IUserElementScoreRepository _userElementScoreRepository;
+		_userElementScoreRepository = userElementScoreRepository;
+	}
 
-		public GetUserElementScoreByUserIdQueryHandler(IUserElementScoreRepository userElementScoreRepository)
-		{
-			_userElementScoreRepository = userElementScoreRepository;
-		}
+	public async Task<UserElementScoreResponseModel> Handle(GetUserElementScoreByUserIdQuery request, CancellationToken cancellationToken)
+	{
+		if (request.UserId <= 0)
+			throw new Exception("User Id cannot be empty");
 
-		public async Task<UserElementScoreResponseModel> Handle(GetUserElementScoreByUserIdQuery request, CancellationToken cancellationToken)
-		{
-			if (request.UserId <= 0)
-				throw new Exception("User Id cannot be empty");
-
-			return await _userElementScoreRepository.GetUserElementScoreByUserIdAsync(request.UserId);
-		}
+		return await _userElementScoreRepository.GetUserElementScoreByUserIdAsync(request.UserId);
 	}
 }
