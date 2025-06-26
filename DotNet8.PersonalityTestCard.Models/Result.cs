4b7c4b699b2df02e1ce22bb34de8c4686a747240
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 12:15:17 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Result.cs
+++ DotNet8.PersonalityTestCard.Models/Result.cs
@@ -72,6 +72,10 @@ public class Result<T>
 
 	#endregion
 
+	#region NotFound
+
 	public static Result<T> NotFound(string message = "No Data Found.") =>
   Result<T>.Failure(message, EnumStatusCode.NotFound);
+
+	#endregion
 }
