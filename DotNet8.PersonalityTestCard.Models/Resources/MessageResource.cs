08db243523e990e3873ac7bc29d3202f8a5081e4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 11:58:37 +0630

    Format Code

--- DotNet8.PersonalityTestCard.Models/Resources/MessageResource.cs
+++ DotNet8.PersonalityTestCard.Models/Resources/MessageResource.cs
@@ -1,19 +1,18 @@
-﻿namespace DotNet8.PersonalityTestCard.Models.Resources
-{
-	public class MessageResource
-	{
+﻿namespace DotNet8.PersonalityTestCard.Models.Resources;
 
-		public static string Success { get; } = "Success.";
-		public static string SaveSuccess { get; } = "Saving Successful.";
-		public static string SaveFail { get; } = "Saving Fail.";
-		public static string UpdateSuccess { get; } = "Updating Successful.";
-		public static string UpdateFail { get; } = "Updating Fail.";
-		public static string DeleteSuccess { get; } = "Deleting Successful.";
-		public static string DeleteFail { get; } = "Deleting Fail.";
-		public static string NotFound { get; } = "No Data Found.";
-		public static string Duplicate { get; } = "Duplicate Data.";
-		public static string InvalidId { get; } = "Id is invalid.";
-		public static string InvalidPageNo { get; } = "Page No is invalid.";
-		public static string InvalidPageSize { get; } = "Page Size is invalid.";
-	}
+public class MessageResource
+{
+
+	public static string Success { get; } = "Success.";
+	public static string SaveSuccess { get; } = "Saving Successful.";
+	public static string SaveFail { get; } = "Saving Fail.";
+	public static string UpdateSuccess { get; } = "Updating Successful.";
+	public static string UpdateFail { get; } = "Updating Fail.";
+	public static string DeleteSuccess { get; } = "Deleting Successful.";
+	public static string DeleteFail { get; } = "Deleting Fail.";
+	public static string NotFound { get; } = "No Data Found.";
+	public static string Duplicate { get; } = "Duplicate Data.";
+	public static string InvalidId { get; } = "Id is invalid.";
+	public static string InvalidPageNo { get; } = "Page No is invalid.";
+	public static string InvalidPageSize { get; } = "Page Size is invalid.";
 }
