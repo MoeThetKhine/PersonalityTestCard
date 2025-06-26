bbb41bb070e7551e76155f74a011aca2190acc01
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 19:32:31 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/User/Command/UpdateUser/UpdateUserCommand.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Command/UpdateUser/UpdateUserCommand.cs
@@ -1,7 +1,11 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Command.UpdateUser;
 
+#region UpdateUserCommand
+
 public class UpdateUserCommand : IRequest<Result<int>>
 {
 	public UserRequestModel userRequestModel { get; set; }
 	public int UserId { get; set; }
 }
+
+#endregion
