6e689f382b3b272a967880224ab91dbb05e0a944
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 11:44:11 +0630

    Feat : Add Enum Status Code

--- DotNet8.PersonalityTestCard.Models/Enums/EnumStatusCode.cs
+++ DotNet8.PersonalityTestCard.Models/Enums/EnumStatusCode.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace DotNet8.PersonalityTestCard.Models.Enums
 {
-	internal class EnumStatusCode
+	public enum EnumStatusCode
 	{
+		None,
+		Success = 200,
+		Created = 201,
+		Accepted = 202,
+		BadRequest = 400,
+		NotFound = 404,
+		Conflict = 409,
+		Locked = 423,
+		InternalServerError = 500
 	}
 }
