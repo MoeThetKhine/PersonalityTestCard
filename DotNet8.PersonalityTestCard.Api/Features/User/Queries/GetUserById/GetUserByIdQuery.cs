8d63d9d2f248f5904551438ee7c14f8ef5adfdca
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 19:32:53 +0630

    -

--- DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQuery.cs
+++ DotNet8.PersonalityTestCard.Api/Features/User/Queries/GetUserById/GetUserByIdQuery.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.PersonalityTestCard.Api.Features.User.Queries.GetUserById;
 
+#region GetUserByIdQuery
+
 public class GetUserByIdQuery : IRequest<Result<UserModel>>
 {
 	public int UserId { get; set; }
 }
+
+#endregion
