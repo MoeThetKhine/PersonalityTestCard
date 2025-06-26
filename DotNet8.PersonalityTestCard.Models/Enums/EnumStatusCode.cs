c165b0a4db835b91b9f1cfec85fe9133b8070035
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 11:59:42 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Enums/EnumStatusCode.cs
+++ DotNet8.PersonalityTestCard.Models/Enums/EnumStatusCode.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.PersonalityTestCard.Models.Enums
+﻿namespace DotNet8.PersonalityTestCard.Models.Enums;
+
+public enum EnumStatusCode
 {
-	public enum EnumStatusCode
-	{
-		None,
-		Success = 200,
-		Created = 201,
-		Accepted = 202,
-		BadRequest = 400,
-		NotFound = 404,
-		Conflict = 409,
-		Locked = 423,
-		InternalServerError = 500
-	}
+	None,
+	Success = 200,
+	Created = 201,
+	Accepted = 202,
+	BadRequest = 400,
+	NotFound = 404,
+	Conflict = 409,
+	Locked = 423,
+	InternalServerError = 500
 }
