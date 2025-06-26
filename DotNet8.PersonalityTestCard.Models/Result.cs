0b4cbbbed7c13204785f16f016097bcbe37c8a0a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-06-26 11:57:08 +0630

    -

--- DotNet8.PersonalityTestCard.Models/Result.cs
+++ DotNet8.PersonalityTestCard.Models/Result.cs
@@ -1,9 +1,4 @@
 ﻿using DotNet8.PersonalityTestCard.Models.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.PersonalityTestCard.Models
 {
